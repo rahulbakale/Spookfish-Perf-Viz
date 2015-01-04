48b5a5f8caf21ba8c2afe86219ac20d900bda201
Rahul Bakale <discourse.on.the.web@gmail.com>
2015-01-04 18:55:52 +0700

    Rename color schemes.

--- src/main/java/spookfishperfviz/HeatMapColorScheme.java
+++ src/main/java/spookfishperfviz/HeatMapColorScheme.java
@@ -20,27 +20,25 @@ import java.util.Arrays;
 import java.util.Objects;
 
 /**
- * @see <a href="http://colorbrewer2.org/">Sequential color schemes from Color Brewer</a>
- * 
  * @author Rahul Bakale
  * @since Dec, 2014
  */
 public enum HeatMapColorScheme {
 
-	NINE_CLASS_YL_OR_RD(ColorPalette.YL_OR_RD, 9),
-	NINE_CLASS_BU_GN(ColorPalette.BU_GN, 9),
-	NINE_CLASS_BU_PU(ColorPalette.BU_PU, 9),
-	NINE_CLASS_PU_BU(ColorPalette.PU_BU, 9),
-	NINE_CLASS_PU_RD(ColorPalette.PU_RD, 9),
-	NINE_CLASS_BLUES(ColorPalette.BLUES, 9),
-	NINE_CLASS_GREENS(ColorPalette.GREENS, 9),
-	EIGHT_CLASS_GREYS(ColorPalette.GREYS, 8),
-	NINE_CLASS_PURPLES(ColorPalette.PURPLES, 9),
-	NINE_CLASS_REDS(ColorPalette.REDS, 9),
-	SIXTEEN_CLASS_CUSTOM_1(ColorPalette.CUSTOM_1, 16),
-	NINETEEN_CLASS_CUSTOM_2(ColorPalette.CUSTOM_2, 19);
+	YELLOW_OR_RED(ColorPalette.COLOR_BREWER_YELLOW_OR_RED, 9),
+	BLUE_GREEN(ColorPalette.COLOR_BREWER_BLUE_GREEN, 9),
+	BLUE_PURPLE(ColorPalette.COLOR_BREWER_BLUE_PURPLE, 9),
+	PURPLE_BLUE(ColorPalette.COLOR_BREWER_PURPLE_BLUE, 9),
+	PURPLE_RED(ColorPalette.COLOR_BREWER_PURPLE_RED, 9),
+	BLUE(ColorPalette.COLOR_BREWER_BLUE, 9),
+	GREEN(ColorPalette.COLOR_BREWER_GREEN, 9),
+	GREY(ColorPalette.COLOR_BREWER_GREY, 8),
+	PURPLE(ColorPalette.COLOR_BREWER_PURPLE, 9),
+	RED(ColorPalette.COLOR_BREWER_RED, 9),
+	RED_2(ColorPalette.INHOUSE_RED_1, 16),
+	RED_3(ColorPalette.INHOUSE_RED_2, 19);
 
-	static final HeatMapColorScheme DEFAULT = SIXTEEN_CLASS_CUSTOM_1;
+	static final HeatMapColorScheme DEFAULT = RED_2;
 
 	private static final String WHITE = "#ffffff";
 
@@ -79,25 +77,28 @@ public enum HeatMapColorScheme {
 		return this.backgroundColor;
 	}
 
+	/**
+	 * @see <a href="http://colorbrewer2.org/">Sequential color schemes from Color Brewer</a>
+	 */
 	private static enum ColorPalette {
 
-		REDS	(new String[] {"#fff5f0", "#fee0d2", "#fcbba1", "#fc9272", "#fb6a4a", "#ef3b2c", "#cb181d", "#a50f15", "#67000d"}),
-		PURPLES	(new String[] {"#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d"}),
-		GREYS	(new String[] {"#ffffff", "#f0f0f0", "#d9d9d9", "#bdbdbd", "#969696", "#737373", "#525252", "#252525", "#000000"}),
-		GREENS	(new String[] {"#f7fcf5", "#e5f5e0", "#c7e9c0", "#a1d99b", "#74c476", "#41ab5d", "#238b45", "#006d2c", "#00441b"}),
-		BLUES	(new String[] {"#f7fbff", "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", "#08306b"}),
-		PU_RD	(new String[] {"#f7f4f9", "#e7e1ef", "#d4b9da", "#c994c7", "#df65b0", "#e7298a", "#ce1256", "#980043", "#67001f"}),
-		PU_BU	(new String[] {"#fff7fb", "#ece7f2", "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"}),
-		BU_PU	(new String[] {"#f7fcfd", "#e0ecf4", "#bfd3e6", "#9ebcda", "#8c96c6", "#8c6bb1", "#88419d", "#810f7c", "#4d004b"}),
-		BU_GN	(new String[] {"#f7fcfd", "#e5f5f9", "#ccece6", "#99d8c9", "#66c2a4", "#41ae76", "#238b45", "#006d2c", "#00441b"}),
-		YL_OR_RD(new String[] {"#ffffcc", "#ffeda0", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#bd0026", "#800026"}),
+		COLOR_BREWER_RED	(new String[] {"#fff5f0", "#fee0d2", "#fcbba1", "#fc9272", "#fb6a4a", "#ef3b2c", "#cb181d", "#a50f15", "#67000d"}),
+		COLOR_BREWER_PURPLE	(new String[] {"#fcfbfd", "#efedf5", "#dadaeb", "#bcbddc", "#9e9ac8", "#807dba", "#6a51a3", "#54278f", "#3f007d"}),
+		COLOR_BREWER_GREY	(new String[] {"#ffffff", "#f0f0f0", "#d9d9d9", "#bdbdbd", "#969696", "#737373", "#525252", "#252525", "#000000"}),
+		COLOR_BREWER_GREEN	(new String[] {"#f7fcf5", "#e5f5e0", "#c7e9c0", "#a1d99b", "#74c476", "#41ab5d", "#238b45", "#006d2c", "#00441b"}),
+		COLOR_BREWER_BLUE	(new String[] {"#f7fbff", "#deebf7", "#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", "#08306b"}),
+		COLOR_BREWER_PURPLE_RED		(new String[] {"#f7f4f9", "#e7e1ef", "#d4b9da", "#c994c7", "#df65b0", "#e7298a", "#ce1256", "#980043", "#67001f"}),
+		COLOR_BREWER_PURPLE_BLUE	(new String[] {"#fff7fb", "#ece7f2", "#d0d1e6", "#a6bddb", "#74a9cf", "#3690c0", "#0570b0", "#045a8d", "#023858"}),
+		COLOR_BREWER_BLUE_PURPLE	(new String[] {"#f7fcfd", "#e0ecf4", "#bfd3e6", "#9ebcda", "#8c96c6", "#8c6bb1", "#88419d", "#810f7c", "#4d004b"}),
+		COLOR_BREWER_BLUE_GREEN		(new String[] {"#f7fcfd", "#e5f5f9", "#ccece6", "#99d8c9", "#66c2a4", "#41ae76", "#238b45", "#006d2c", "#00441b"}),
+		COLOR_BREWER_YELLOW_OR_RED	(new String[] {"#ffffcc", "#ffeda0", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#bd0026", "#800026"}),
 		
-		CUSTOM_1(new String[] {"#FFE6E6", "#FFCCCC", "#FFB2B2", "#FF9999", "#FF8080", "#FF6666", "#FF4D4D", "#FF3333", "#FF1919", 
-							   "#FF0000", "#E60000", "#CC0000", "#B20000", "#990000", "#800000", "#660000"}),
+		INHOUSE_RED_1(new String[] {"#FFE6E6", "#FFCCCC", "#FFB2B2", "#FF9999", "#FF8080", "#FF6666", "#FF4D4D", "#FF3333", "#FF1919", 
+							   		"#FF0000", "#E60000", "#CC0000", "#B20000", "#990000", "#800000", "#660000"}),
 		
-		CUSTOM_2(new String[] {"#FFE5E5", "#FFCCCC", "#FFB2B2", "#FF9999", "#FF7F7F", "#FF6666", "#FF4C4C", "#FF3232", "#FF1919",
-				 			   "#FF0000", "#E50000", "#CC0000", "#B20000", "#990000", "#7F0000", "#660000", "#4C0000", "#330000", 
-				 			   "#190000"});
+		INHOUSE_RED_2(new String[] {"#FFE5E5", "#FFCCCC", "#FFB2B2", "#FF9999", "#FF7F7F", "#FF6666", "#FF4C4C", "#FF3232", "#FF1919",
+				 			   		"#FF0000", "#E50000", "#CC0000", "#B20000", "#990000", "#7F0000", "#660000", "#4C0000", "#330000", 
+				 			   		"#190000"});
 
 		private final String[] colors;
 
